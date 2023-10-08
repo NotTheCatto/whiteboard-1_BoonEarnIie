d71d4bfc3d6c50c9f5b2ea12f402580a62b29a1f
NotTheCatto <129914663+NotTheCatto@users.noreply.github.com>
2023-10-08 14:32:58 +0800

    Add initialization and display array

---
+++ p1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class p1 {
+    public static void main(String[] args) {
+        // Initialize input values
+        int[] input = {21, 400, 8, -3, 77, 99, -16, 55, 111, -36, 28 };
+
+        // Begin sorting
+
+
+
+        // Display output results
+        System.out.print(Arrays.toString(input));
+    }
+}

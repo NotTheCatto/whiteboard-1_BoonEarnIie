504222e7d11794797e396ccb75fbbce8ea3e9c62
NotTheCatto <129914663+NotTheCatto@users.noreply.github.com>
2023-10-29 18:24:44 +0800

    Initialize lists for problem 5

---
+++ p5.java
@@ -0,0 +1,8 @@
+public class p5 {
+    public static void main(String[] args) {
+        // Initialize list 1 and list 2
+        int[] list1 = { 4, 5, 2, 3, 1, 6 };
+        int[] list2 = { 8, 7, 6, 9, 4, 5 };
+
+    }
+}

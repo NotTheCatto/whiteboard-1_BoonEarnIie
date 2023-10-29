9bb42630a87cf261376babd47c95b6d0f8ed04c6
NotTheCatto <129914663+NotTheCatto@users.noreply.github.com>
2023-10-29 22:35:26 +0800

    Add initialization and input prompts for problem 6

---
+++ p6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class p6 {
+    public static void main(String[] args) {
+        // Initialize objects and variables
+        Scanner input = new Scanner(System.in);
+        String inputStr;
+
+        // Prompt string
+        System.out.print("Enter a string : ");
+        inputStr = input.nextLine();
+
+    }
+}

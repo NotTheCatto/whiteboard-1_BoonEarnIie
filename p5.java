963725e2e84900ee18add9e644545cebe866f328
NotTheCatto <129914663+NotTheCatto@users.noreply.github.com>
2023-10-29 18:30:09 +0800

    Added logic to check symmetric differences
    Value could be sorted based on the sample expected output, but since the question is for testing this particular logic, I will try to keep the code short.

--- p5.java
+++ p5.java
@@ -1,8 +1,36 @@
 public class p5 {
     public static void main(String[] args) {
-        // Initialize list 1 and list 2
+        // Initialize list 1 and list 2 and variables
         int[] list1 = { 4, 5, 2, 3, 1, 6 };
         int[] list2 = { 8, 7, 6, 9, 4, 5 };
+        boolean checkRepeat = false;
 
+        // Check repeated values from list 1
+        for (int i : list1) {
+            checkRepeat = false;
+            for (int j : list2) {
+                if (i == j) {
+                    checkRepeat = true;
+                }
+            }
+            if (checkRepeat == false) {
+                // Difference found, display values
+                System.out.println(i);
+            }
+        }
+
+        // Check repeated values from list 2
+        for (int i : list2) {
+            checkRepeat = false;
+            for (int j : list1) {
+                if (i == j) {
+                    checkRepeat = true;
+                }
+            }
+            if (checkRepeat == false) {
+                // Difference found, display values
+                System.out.println(i);
+            }
+        }
     }
 }

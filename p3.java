762bef16f06a57710434301329196885d9783be9
NotTheCatto <129914663+NotTheCatto@users.noreply.github.com>
2023-10-29 17:05:27 +0800

    Added logic for generating Fibonacci sequence
    First, the program prompts for the first 2 element and the number of elements to be generated.
    
    Then, the program display the first two elements before proceeding to generate the next few elements by passing the element that comes before the next element and the limit to control the number of generated elements.
    
    In the Fibonacci function, the recursion stops when the limit will be 0 before return the last element.

---
+++ p3.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class p3 {
+    public static void main(String[] args) {
+        // Initialize variables and objects
+        Scanner input = new Scanner(System.in);
+        int n1 = 0, n2 = 0, i = 0;
+
+        // Prompt input
+        try {
+            System.out.print("Enter 1st number of the fibonacci sequence > ");
+            n1 = input.nextInt();
+        } catch (Exception e) {
+            System.out.print("Invalid input for 1st number\n");
+            input.next();
+        }
+
+        try {
+            System.out.print("Enter 2nd number of the fibonacci sequence > ");
+            n2 = input.nextInt();
+        } catch (Exception e) {
+            System.out.print("Invalid input for 2nd number\n");
+            input.next();
+        }
+
+        try {
+            System.out.print("Enter number of elements to generate > ");
+            i = input.nextInt();
+        } catch (Exception e) {
+            System.out.print("Invalid input for number of elements\n");
+            input.next();
+        }
+
+        // Process and dispaly results
+        System.out.println(n1 + ", \n" + n2 + ", ");
+        System.out.println(fibonacci(n1, n2, i));
+
+    }
+
+    public static int fibonacci(int i, int j, int limit) {
+        if (limit - 1 > 0) {
+            System.out.println(i + j + ", ");
+            return fibonacci(j, i + j, limit - 1);
+        } else {
+            return i + j;
+        }
+    }
+
+}

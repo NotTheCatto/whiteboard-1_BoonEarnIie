92cbac7266d71e6deb7a014efb299d19c77f8d4e
NotTheCatto <129914663+NotTheCatto@users.noreply.github.com>
2023-10-29 17:47:36 +0800

    Add logic to check intersection
    The logic check if there are matching values, if there are matching value, the value will be displayed.

--- p4.java
+++ p4.java
@@ -1,13 +1,17 @@
-import java.util.*;
-
 public class p4 {
     public static void main(String[] args) {
         // Initialize list 1 and list 2
         int[] list1 = { 4, 5, 2, 3, 1, 6 };
         int[] list2 = { 8, 7, 6, 9, 4, 5 };
 
-        // Display lists' values
-        System.out.print(Arrays.toString(list1));
-        System.out.print(Arrays.toString(list2));
+        // Check intersections
+        for (int i : list1) {
+            for (int j : list2) {
+                if (i == j){
+                    System.out.println(i);
+                }
+            }
+        }
+
     }
 }

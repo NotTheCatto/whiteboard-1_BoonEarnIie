b551036b73d4b814c6e32563afd2ac840d5d31f6
NotTheCatto <129914663+NotTheCatto@users.noreply.github.com>
2023-10-29 17:08:54 +0800

    Initialize lists for problem 4

---
+++ p4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class p4 {
+    public static void main(String[] args) {
+        // Initialize list 1 and list 2
+        int[] list1 = { 4, 5, 2, 3, 1, 6 };
+        int[] list2 = { 8, 7, 6, 9, 4, 5 };
+
+        // Display lists' values
+        System.out.print(Arrays.toString(list1));
+        System.out.print(Arrays.toString(list2));
+    }
+}
